b3c3cd9383242c689e76c86efd7105f47db7f5d4
jdsinniah <dario.sinniah@hotmail.it>
2021-02-24 09:39:20 +0100

    [JDSinniah] Molitplicazione e divisione per 2 tramite operatore di shift.

---
+++ P2_01_02_AC/Exercises/shift_operations/cmake-build-debug/CMakeFiles/shift_operations.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shift_operations.dir/main.c.o"
+  "shift_operations"
+  "shift_operations.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/shift_operations.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ P2_01_02_AC/Exercises/shift_operations/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    short n = 11;
+    printf("n >> 1 = %d\n", n >> 1);
+    printf("n >> 2 = %d\n", n >> 2);
+    printf("n >> 3 = %d\n", n >> 3);
+    printf("n << 1 = %d\n", n << 1);
+    printf("n << 2 = %d\n", n << 2);
+    printf("n << 3 = %d\n", n << 1);
+    return 0;
+}
