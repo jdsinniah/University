177da398051864fde37b3600374aa8d813fb3372
jdsinniah <dario.sinniah@hotmail.it>
2021-02-28 00:11:00 +0100

    [JDSinniah] Finished base conversion.

--- 2 -Sitemi aritmetici di un computer: Tipo Intero/P2_02_01_AT/Exercises/base_10_to_2/main.c
+++ 2 -Sitemi aritmetici di un computer: Tipo Intero/P2_02_01_AT/Exercises/base_10_to_2/main.c
@@ -1,25 +1,52 @@
 #include <stdio.h>
 
+void classic_base_10_to_2_converter(unsigned int value, int bits[], unsigned int bits_N);
+void bitwise_base_10_to_2_converter(unsigned int value, int bits[], unsigned int bits_N);
+void show_bits(int bits[], unsigned int bits_N);
+
 int main() {
-    printf("Hello, World!\n");
+    int bits[16];
+    classic_base_10_to_2_converter(16000000, bits, 16);
+    show_bits(bits, 16);
+    bitwise_base_10_to_2_converter(16000000, bits, 16);
+    show_bits(bits, 16);
     return 0;
 }
 
-void classic_base_10_to_2_converter(unsigned int value, char bits[]) {
+void classic_base_10_to_2_converter(unsigned int value, int bits[], unsigned int bits_N) {
     unsigned int remainder;
-    if(value == 0) {
-        return 0;
-    }
+
+    int bits_index = bits_N-1;
     do {
         if(value >= 0) {
             remainder = value % 2;
             value /= 2;
-            bits[counter] = value;
+            bits[bits_index] = remainder;
+        } else {
+            bits[bits_index] = 0;
         }
-        bits[counter] =
-    } while (value != 0)
+        bits_index--;
+    } while (bits_index >= 0);
 }
 
-void bitwise_base_10_to_2_converter(unsigned int value, char bits[]) {
+void bitwise_base_10_to_2_converter(unsigned int value, int bits[], unsigned int bits_N) {
+    unsigned int remainder;
 
+    int bits_index = bits_N-1;
+    do {
+        if(value >= 0) {
+            remainder = value - ((value >> 1) * 2);
+            value >>= 1;
+            bits[bits_index] = remainder;
+        } else {
+            bits[bits_index] = 0;
+        }
+        bits_index--;
+    } while (bits_index >= 0);
+}
+
+void show_bits(int bits[], unsigned int bits_N) {
+    for(int i = 0; i < bits_N; i++)
+        printf("%d", bits[i]);
+    printf("\n");
 }
