c9c76bd310a1a0493ed36a60ab7ac0be0b6f82df
jdsinniah <dario.sinniah@hotmail.it>
2021-03-03 08:28:23 +0100

    [JDSinniah] Strings and pointers.

--- Programmazione 2/4 - Stringhe/P2_04_01/Exercises/string/main.c
+++ Programmazione 2/4 - Stringhe/P2_04_01/Exercises/string/main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+char *mesi[] = {
+        "Numero di mese non corretto",
+        "Gennaio",
+        "Febbraio",
+        "Marzo",
+        "Aprile",
+        "Maggio",
+        "Giugno",
+        "Luglio",
+        "Agosto",
+        "Settembre",
+        "Ottobre",
+        "Novembre",
+        "Dicembre"
+};
+
+void nome_mese();
+
 int main() {
     char a = 'a'; char A[] = "a";
     printf("sizeof(constant char)   = %d\tvalue = %d\n", sizeof(a), a);
@@ -7,5 +25,45 @@ int main() {
     printf("sizeof(string array)    = %d\tvalue = %d, %d\n", sizeof(A), A[0], A[1]);
     puts("\n");
     printf("\t constant string:\ncontenuto = %d, %d\tindirizzo = %u\n", *"a", *("a" + 1), "a");
+    puts("\n");
+    nome_mese();
     return 0;
+}
+
+void nome_mese() {
+    int n, riga, j; char mese_di_n[30];
+    char mesi[][30] = {
+            "Numero di mese non corretto",
+            "Gennaio",
+            "Febbraio",
+            "Marzo",
+            "Aprile",
+            "Maggio",
+            "Giugno",
+            "Luglio",
+            "Agosto",
+            "Settembre",
+            "Ottobre",
+            "Novembre",
+            "Dicembre"
+    };
+    printf("numero mese = "); scanf("%d", &n);
+    if(n < 1 || n > 12)
+        riga = 0;
+    else
+        riga = n;
+    for (j = 0; j < 30; j++)
+        mese_di_n[j] = mesi[riga][j];
+    printf("mese corrispondente = %s\n", mese_di_n);
+}
+
+void nome_mese_puntatori() {
+    int n, riga, j; char *mese_di_n;
+    printf("numero mese = "); scanf("%d", &n);
+    if(n < 1 || n > 12)
+        riga = 0;
+    else
+        riga = n;
+    mese_di_n = mesi[riga];
+    printf("mese corrispondente = %s\n", mese_di_n);
 }
