c032a17906b64c6bd3808d406d794265b2117f90
jdsinniah <dario.sinniah@hotmail.it>
2021-03-06 23:34:52 +0100

    [JDSinniah] More string stuff.

--- Programmazione 2/4 - Stringhe/P2_04_01_AC/Exercises/input_stream/main.c
+++ Programmazione 2/4 - Stringhe/P2_04_01_AC/Exercises/input_stream/main.c
@@ -12,11 +12,15 @@ int main() {
 //    buffer[j - 1] = '\0';
 //    printf("La stringa %s e' lunga %lu caratteri\n", buffer, strlen(buffer));
 
-//    char stringa[] = "ciao";
     char* stringa = "ciao";
-    puts(stringa);
-    *stringa = 'm';
-    puts(stringa);
+    printf("stringa[4] = %d",*(stringa+4));
+//    for (int i = 0; i < strlen(stringa) + 2; i++)
+//        printf("%c", stringa[i]);
+//    char* stringa = "ciao";
+//    puts(stringa);
+//    *stringa = 'm';
+//    puts(stringa);
+    printf("\n");
 
     return 0;
 }
